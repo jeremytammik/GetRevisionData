59354308ba86471ad09f3980aec9d773c8413021
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-05-29 15:11:06 +0200

    Revit 2015 version up and running in utter perfection

--- GetRevisionData/Command.cs
+++ GetRevisionData/Command.cs
@@ -1,237 +1,38 @@
-﻿using System;
+#region Namespaces
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Collections;
-using Autodesk.Revit;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.DB.Events;
-using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Windows;
+using TaskDialog = Autodesk.Revit.UI.TaskDialog;
+#endregion
 
-namespace NBBJ.NBBJProjectRevisionsTest.CS
+namespace GetRevisionData
 {
-  [Regeneration( RegenerationOption.Manual )]
-  [Transaction( TransactionMode.Manual )]
-
+  /// <summary>
+  /// External command demonstrating how to use the 
+  /// Revit 2015 Revision API to retrieve and display
+  /// all infoormation shown in the Revit 
+  /// 'Sheet Issues/Revisions' dialogue.
+  /// </summary>
+  [Transaction( TransactionMode.ReadOnly )]
   public class Command : IExternalCommand
   {
-    // the active Revit application
-    public static UIApplication m_app;
-
-    /*------------------------------------------------------------------------------------**/
-    /// <author>Dan.Tartaglia </author>                              <date>03/2010</date>
-    /*--------------+---------------+---------------+---------------+---------------+------*/
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements )
-    {
-      // Get the application
-      m_app = commandData.Application;
-
-      // Get the active document
-      Document doc = m_app.ActiveUIDocument.Document;
-
-      // Display a message and exit if the active document is a family
-      if( doc.IsFamilyDocument )
-      {
-        MessageBox.Show( "This utility should only be utilized when a Revit RVT file is active",
-            "A Revit RVT file is not active.", MessageBoxButtons.OK, MessageBoxIcon.Information );
-        return Result.Failed;
-      }
-
-      // Display a message and exit if the active view is not a sheet
-      if( !( doc.ActiveView is ViewSheet ) )
-      {
-        MessageBox.Show( "This utility should only be utilized in a sheet view",
-            "Active view is not a sheet.", MessageBoxButtons.OK, MessageBoxIcon.Information );
-        return Result.Failed;
-      }
-
-      ProcessStart( doc );
-
-      return Result.Succeeded;
-    }
-
-    public void ProcessStart( Document doc )
-    {
-      TextWriter tw = null;
-
-      try
-      {
-        ViewSheet oViewSheet = doc.ActiveView
-          as ViewSheet;
-
-        if( oViewSheet == null )
-          return;
-
-        // Create a writer and open the file
-
-        tw = new StreamWriter(
-          "C:/tmp/RevisionTest.txt" );
-
-        ParamsFromGetAllRevElements(
-          oViewSheet, tw, doc );
-
-        // Close the text file stream
-
-        tw.Close();
-      }
-      catch
-      {
-        // Close the text file stream
-
-        tw.Close();
-
-        return;
-      }
-    }
-
-    public void ParamsFromGetAllRevElements(
-      ViewSheet oViewSheet,
-      TextWriter tw,
-      Document doc )
-    {
-      try
-      {
-        IList<ElementId> oElemIDs
-          = oViewSheet.GetAllProjectRevisionIds();
-
-        if( oElemIDs.Count == 0 )
-          return;
-
-        foreach( ElementId elemID in oElemIDs )
-        {
-          Element oEl = doc.GetElement( elemID );
-
-          bool blnHidden = oEl.IsHidden( oViewSheet );
-
-          // Add text line to text file
-          tw.WriteLine( "Hidden = "
-            + blnHidden.ToString() );
-
-          // Add text line to text file
-          tw.WriteLine( "Element Name: "
-            + oEl.Name );
-
-          Parameter oParamRevEnum = oEl.get_Parameter(
-            BuiltInParameter.PROJECT_REVISION_ENUMERATION );
-
-          if( oParamRevEnum != null )
-            // Add text line to text file
-            tw.WriteLine( "oParamRevEnum = "
-              + GetParameterInformation(
-                oParamRevEnum, doc ) );
-          else
-            // Add text line to text file
-            tw.WriteLine( "oParamRevEnum = null" );
-
-          Parameter oParamRevDate = oEl.get_Parameter(
-            BuiltInParameter.PROJECT_REVISION_REVISION_DATE );
-
-          if( oParamRevDate != null )
-            // Add text line to text file
-            tw.WriteLine( "oParamRevDate = "
-              + GetParameterInformation(
-                oParamRevDate, doc ) );
-          else
-            // Add text line to text file
-            tw.WriteLine( "oParamRevDate = null" );
-
-          Parameter oParamRevDescrip = oEl.get_Parameter(
-            BuiltInParameter.PROJECT_REVISION_REVISION_DESCRIPTION );
-
-          if( oParamRevDescrip != null )
-            // Add text line to text file
-            tw.WriteLine( "oParamRevDescrip = "
-              + GetParameterInformation(
-                oParamRevDescrip, doc ) );
-          else
-            // Add text line to text file
-            tw.WriteLine( "oParamRevDescrip = null" );
-
-          Parameter oParamRevIssued = oEl.get_Parameter(
-            BuiltInParameter.PROJECT_REVISION_REVISION_ISSUED );
-
-          if( oParamRevIssued != null )
-            // Add text line to text file
-            tw.WriteLine( "oParamRevIssued = "
-              + GetParameterInformation(
-                oParamRevIssued, doc ) );
-          else
-            // Add text line to text file
-            tw.WriteLine( "oParamRevIssued = null" );
-
-          Parameter oParamRevIssuedBy = oEl.get_Parameter(
-            BuiltInParameter.PROJECT_REVISION_REVISION_ISSUED_BY );
-
-          if( oParamRevIssuedBy != null )
-            // Add text line to text file
-            tw.WriteLine( "oParamRevIssuedBy = "
-              + GetParameterInformation(
-                oParamRevIssuedBy, doc ) );
-          else
-            // Add text line to text file
-            tw.WriteLine( "oParamRevIssuedBy = null" );
-
-          Parameter oParamRevIssuedTo = oEl.get_Parameter(
-            BuiltInParameter.PROJECT_REVISION_REVISION_ISSUED_TO );
-
-          if( oParamRevIssuedTo != null )
-            // Add text line to text file
-            tw.WriteLine( "oParamRevIssuedTo = "
-              + GetParameterInformation(
-                oParamRevIssuedTo, doc ) );
-          else
-            // Add text line to text file
-            tw.WriteLine( "oParamRevIssuedTo = null" );
-
-          Parameter oParamRevNumber = oEl.get_Parameter(
-            BuiltInParameter.PROJECT_REVISION_REVISION_NUM );
-
-          if( oParamRevNumber != null )
-            // Add text line to text file
-            tw.WriteLine( "oParamRevNumber = "
-              + GetParameterInformation(
-                oParamRevNumber, doc ) );
-          else
-            // Add text line to text file
-            tw.WriteLine( "oParamRevNumber = null" );
-
-          Parameter oParamSeqNumber = oEl.get_Parameter(
-            BuiltInParameter.PROJECT_REVISION_SEQUENCE_NUM );
-
-          if( oParamSeqNumber != null )
-            // Add text line to text file
-            tw.WriteLine( "oParamSeqNumber = "
-              + GetParameterInformation(
-                oParamSeqNumber, doc ) );
-          else
-            // Add text line to text file
-            tw.WriteLine( "oParamSeqNumber = null" );
-
-          // Add text line to text file
-          tw.WriteLine( "==============================" );
-        }
-      }
-      catch
-      {
-      }
-    }
-
-    /*------------------------------------------------------------------------------------**/
+    #region Unused parameter value to display string converter
     /// <summary>
-    /// Extract the parameter information 
+    /// Extract the parameter information.
+    /// By Dan Tartaglia.
     /// </summary>
-    /// <returns> string </returns>
-    /// <author>Dan.Tartaglia </author>                              <date>04/2010</date>
-    /*--------------+---------------+---------------+---------------+---------------+------*/
     public string GetParameterInformation(
       Parameter para,
-      Document document )
+      Document doc )
     {
       string defName = "";
 
@@ -256,14 +57,10 @@ namespace NBBJ.NBBJProjectRevisionsTest.CS
           Autodesk.Revit.DB.ElementId id
             = para.AsElementId();
 
-          if( id.IntegerValue >= 0 )
-          {
-            defName = document.GetElement( id ).Name;
-          }
-          else
-          {
-            defName = id.IntegerValue.ToString();
-          }
+          defName = ( id.IntegerValue >= 0 )
+            ? doc.GetElement( id ).Name
+            : id.IntegerValue.ToString();
+
           break;
 
         case StorageType.Integer:
@@ -294,6 +91,117 @@ namespace NBBJ.NBBJProjectRevisionsTest.CS
           break;
       }
       return defName;
+    }
+    #endregion // Unused parameter value to display string converter
+
+    /// <summary>
+    /// A container for the revision data displayed in
+    /// the Revit 'Sheet Issues/Revisions' dialogue.
+    /// </summary>
+    class RevisionData
+    {
+      public int Sequence { get; set; }
+      public RevisionNumberType Numbering { get; set; }
+      public string Date { get; set; }
+      public string Description { get; set; }
+      public bool Issued { get; set; }
+      public string IssuedTo { get; set; }
+      public string IssuedBy { get; set; }
+      public RevisionVisibility Show { get; set; }
+
+      public RevisionData( Revision r )
+      {
+        Sequence = r.SequenceNumber;
+        Numbering = r.NumberType;
+        Date = r.RevisionDate;
+        Description = r.Description;
+        Issued = r.Issued;
+        IssuedTo = r.IssuedTo;
+        IssuedBy = r.IssuedBy;
+        Show = r.Visibility;
+      }
+    }
+
+    /// <summary>
+    /// Generate a Windows modeless form on the fly 
+    /// and display the revision data in it in a 
+    /// DataGridView.
+    /// </summary>
+    void DisplayRevisionData(
+      List<RevisionData> revision_data,
+      IWin32Window owner )
+    {
+      System.Windows.Forms.Form form
+        = new System.Windows.Forms.Form();
+
+      form.Size = new Size( 680, 180 );
+      form.Text = "Revision Data";
+
+      DataGridView dg = new DataGridView();
+      dg.DataSource = revision_data;
+      dg.AllowUserToAddRows = false;
+      dg.AllowUserToDeleteRows = false;
+      dg.AllowUserToOrderColumns = true;
+      dg.Dock = System.Windows.Forms.DockStyle.Fill;
+      dg.Location = new System.Drawing.Point( 0, 0 );
+      dg.ReadOnly = true;
+      dg.TabIndex = 0;
+      dg.Parent = form;
+      dg.AutoSize = true;
+      dg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+      dg.AutoResizeColumns( DataGridViewAutoSizeColumnsMode.AllCells );
+      dg.AutoResizeColumns();
+
+      form.ShowDialog( owner );
+    }
+
+    public Result Execute(
+      ExternalCommandData commandData,
+      ref string message,
+      ElementSet elements )
+    {
+      IWin32Window revit_window
+        = new JtWindowHandle(
+          ComponentManager.ApplicationWindow );
+
+      UIApplication uiapp = commandData.Application;
+      UIDocument uidoc = uiapp.ActiveUIDocument;
+      Document doc = uidoc.Document;
+
+      if( doc.IsFamilyDocument )
+      {
+        TaskDialog.Show( "Not a Revit RVT Project",
+          "This command requires an active Revit RVT file." );
+
+        return Result.Failed;
+      }
+
+      if( !( doc.ActiveView is ViewSheet ) )
+      {
+        TaskDialog.Show( "Current View is not a Sheet",
+          "This command requires an active sheet view." );
+        return Result.Failed;
+      }
+
+      IList<ElementId> ids
+        = Revision.GetAllRevisionIds( doc );
+
+      int n = ids.Count;
+
+      List<RevisionData> revision_data
+        = new List<RevisionData>( n );
+
+      foreach( ElementId id in ids )
+      {
+        Revision r = doc.GetElement( id ) as Revision;
+
+        revision_data.Add( new RevisionData( r ) );
+      }
+
+      DisplayRevisionData( revision_data,
+        revit_window );
+
+      return Result.Succeeded;
     }
   }
 }
